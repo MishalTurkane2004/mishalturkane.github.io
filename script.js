263f470de82b8c7f238c35b8963ad8b5a7a75cd6
Mishal Turkane <mishalturkane@gmail.com>
2025-02-02 16:27:23 +0530

    code updated

--- script.js
+++
@@ -1,8 +0,0 @@
-// URL se .html hata kar update karna
-window.onload = function () {
-    if (window.location.pathname.endsWith(".html")) {
-      let newPath = window.location.pathname.replace(".html", "");
-      window.history.replaceState(null, "", newPath);
-    }
-  };
-  
